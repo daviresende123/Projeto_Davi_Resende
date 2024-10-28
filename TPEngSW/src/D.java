27c57fefcc5229169f2294d209ce2d74e909059a
Davi Resende <davi.resende@estudante.ufla.br>
2024-10-28 13:59:00 -0300

    Adicionada a classe D

---
+++ TPEngSW/src/D.java
@@ -0,0 +1,32 @@
+public class D {
+    private int D1;
+    private float D2;
+
+    //Métodos set
+    public void setD1(int D1){
+        this.D1 = D1;
+    }
+
+    public void setD2(float D2){
+        this.D2 = D2;
+    }
+
+
+    //Métodos get
+    public int getD1(){
+        return D1;
+    }
+
+    public float getD2(){
+        return D2;
+    }
+
+    //Métodos M
+    public void MD1(){
+        System.out.println("MD1");
+    }
+
+    public void MD2(){
+        System.out.println("MD2");
+    }
+}
