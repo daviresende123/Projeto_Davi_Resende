a94ae67e3e57b9a52a28176a99db799fc3dcedaf
Davi Resende <davi.resende@estudante.ufla.br>
2024-10-28 12:41:38 -0300

    Adicionadas as classes A e B

---
+++ TPEngSW/src/A.java
@@ -0,0 +1,31 @@
+public class A{
+    private int A1;
+    private float A2;
+
+    //Métodos set
+    public void setA1(int A1){
+        this.A1 = A1;
+    }
+
+    public void setA2(float A2){
+        this.A2 = A2;
+    }
+
+    //Métodos get
+    public int getA1(){
+        return A1;
+    }
+
+    public float getA2(){
+        return A2;
+    }
+
+    //Métodos M
+    public void MA1(){
+        System.out.println("Rodando metodo MA1");
+    }
+
+    public void MA2(){
+        System.out.println("Rodando metodo MA2");
+    }
+}
---
+++ TPEngSW/src/B.java
@@ -0,0 +1,32 @@
+public class B {
+    private int B1;
+    private float B2;
+
+    //Métodos set
+    public void setB1(int B1){
+        this.B1 = B1;
+    }
+
+    public void setA2(float B2){
+        this.B2 = B2;
+    }
+
+    //Métodos get
+    public int getB1(){
+        return B1;
+    }
+
+    public float getB2(){
+        return B2;
+    }
+
+    //Métodos M
+    public void BA1(){
+        System.out.println("Rodando metodo BA1");
+    }
+
+    public void BA2(){
+        System.out.println("Rodando metodo BA2");
+    }
+
+}
